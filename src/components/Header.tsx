feea573d2be09337a787ef00b8cfebfe9ce0b562
ElmiraZeynalova <150475421+ElmiraZeynalova@users.noreply.github.com>
2026-03-15 10:06:45 +0100

    style changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,21 +6,21 @@ export default function Header(){
     return(
         <header>
             <NavLink to="/workouts/new">
-                <svg className="header-btn" x="0px" y="0px" width="22" height="22" strokeWidth="1.2" viewBox="0 0 24 24"> 
-                    <line x1="0" y1="12" x2="20" y2="12" stroke="black" strokeWidth="1.5"/>
-                    <line x1="10" y1="2" x2="10" y2="22" stroke="black" strokeWidth="1.5"/>
+                <svg className="header-btn" x="0px" y="0px" width="22" height="22" strokeWidth="1.8" viewBox="0 0 24 24"> 
+                    <line x1="0" y1="12" x2="20" y2="12" stroke="black" strokeWidth="1.8"/>
+                    <line x1="10" y1="2" x2="10" y2="22" stroke="black" strokeWidth="1.8"/>
                 </svg>
             </NavLink>
             <p className="date">{dayjs(selectedDate).format('MMMM D')}</p>
             <NavLink className="header-btn" to="/calendar">
                 <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="-2 -2 18 18" fill="currentColor">
 
-                    <rect x="0.5" y="0.5" width="14" height="14" rx="3.2" ry="3.2" fill="none"  stroke="black" strokeWidth="1.2"/>
+                    <rect x="0.5" y="0.5" width="14" height="14" rx="3.2" ry="3.2" fill="none"  stroke="black" strokeWidth="1.5"/>
 
-                    <line x1="0" y1="4.2" x2="15" y2="4.2" stroke="black" strokeWidth="1.4"/>
+                    <line x1="0" y1="4.2" x2="15" y2="4.2" stroke="black" strokeWidth="1.5"/>
 
-                    <rect x="3.8" y="-0.7" width="1.4" height="2.5" rx="0.6" fill="black"/>
-                    <rect x="10.2" y="-0.7" width="1.4" height="2.5" rx="0.6" fill="black"/>
+                    <rect x="3.8" y="-0.7" width="1.5" height="2.5" rx="0.6" fill="black"/>
+                    <rect x="10.2" y="-0.7" width="1.5" height="2.5" rx="0.6" fill="black"/>
 
                     <circle cx="4" cy="8" r="0.8" fill="black"/>
                     <circle cx="7.5" cy="8" r="0.8" fill="black"/>
