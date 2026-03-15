1355f8cd12af3a2da5a5017f796dfc1725749c22
ElmiraZeynalova <150475421+ElmiraZeynalova@users.noreply.github.com>
2026-03-15 10:02:40 +0100

    style changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,14 +13,14 @@ export default function Header(){
             </NavLink>
             <p className="date">{dayjs(selectedDate).format('MMMM D')}</p>
             <NavLink className="header-btn" to="/calendar">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="-2 -2 18 18" fill="currentColor">
+                <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="-2 -2 18 18" fill="currentColor">
 
-                    <rect x="0.5" y="0.5" width="14" height="14" rx="3.2" ry="3.2" fill="none"  stroke="black" strokeWidth="1"/>
+                    <rect x="0.5" y="0.5" width="14" height="14" rx="3.2" ry="3.2" fill="none"  stroke="black" strokeWidth="1.2"/>
 
-                    <line x1="0" y1="4.2" x2="15" y2="4.2" stroke="black" strokeWidth="1.2"/>
+                    <line x1="0" y1="4.2" x2="15" y2="4.2" stroke="black" strokeWidth="1.4"/>
 
-                    <rect x="3.8" y="-0.7" width="1.2" height="2.5" rx="0.6" fill="black"/>
-                    <rect x="10.2" y="-0.7" width="1.2" height="2.5" rx="0.6" fill="black"/>
+                    <rect x="3.8" y="-0.7" width="1.4" height="2.5" rx="0.6" fill="black"/>
+                    <rect x="10.2" y="-0.7" width="1.4" height="2.5" rx="0.6" fill="black"/>
 
                     <circle cx="4" cy="8" r="0.8" fill="black"/>
                     <circle cx="7.5" cy="8" r="0.8" fill="black"/>
