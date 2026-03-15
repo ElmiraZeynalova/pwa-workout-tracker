911286da2163da268b9893c8dd0ced6ed446e17a
ElmiraZeynalova <150475421+ElmiraZeynalova@users.noreply.github.com>
2026-03-15 10:50:26 +0100

    style changes

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5119daf5'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.82k4et2fo3"
+    "revision": "0.fqpo1nndlm8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
