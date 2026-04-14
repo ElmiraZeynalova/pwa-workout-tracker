b4ef611915852cef2c79e775655004d5b05ba837
ElmiraZeynalova <150475421+ElmiraZeynalova@users.noreply.github.com>
2026-04-14 21:45:00 +0200

    Infinite calendar page created

--- src/components/CalendarMonth.tsx
+++ src/components/CalendarMonth.tsx
@@ -8,14 +8,13 @@ interface Props {
   offset: number
   selectedDate: string
   onSelectedDate: (date: string) => void
-  innerRef?: React.RefObject<HTMLDivElement | null>
 }
 
-export default function CalendarMonth({ monthName, dates, offset, selectedDate, onSelectedDate, innerRef }: Props){
+export default function CalendarMonth({ monthName, dates, offset, selectedDate, onSelectedDate}: Props){
     const workouts = useRenderWorkoutOnScreenStore((state) => state.workouts)
     
     return(
-        <div className="month" ref={innerRef}>
+        <div className="calendar-month">
             <h1>{monthName}</h1>
             <div className="dates">
                 {Array.from({ length: offset }, (_, i) => (
@@ -26,15 +25,18 @@ export default function CalendarMonth({ monthName, dates, offset, selectedDate, 
                         <span className="today-label">
                             {dayjs().format("YYYY-MM-DD") === date ? "TODAY" : ""}
                         </span>
-                        <div
-                            className={clsx("number", selectedDate === date && "selected")}
-                            onClick={() => onSelectedDate(date)}
-                        >
-                            {dayjs(date).format("D")}
+                        <div className="number-wrapper">
+                            { selectedDate === date && <div className="calendar-selector"></div>}
+                            <div
+                                className={clsx("number", selectedDate === date && "selected")}
+                                onClick={() => onSelectedDate(date)}
+                            >
+                                {dayjs(date).format("D")}
+                            </div>
                         </div>
                         {workouts[date]
-                            ? <div className="workout-day-marker" />
-                            : <div style={{ width: 5, height: 5 }} />
+                            ? <div className="workout-day-marker" style={selectedDate === date ? { transform: "translateY(8px)" } : undefined}/>
+                            : <div style={{ width: 7, height: 7 }} />
                         }
                     </div>
                 ))}
--- src/components/CalendarPage.tsx
+++ src/components/CalendarPage.tsx
@@ -2,33 +2,73 @@ import { FaChevronLeft } from "react-icons/fa";
 import {NavLink} from "react-router-dom"
 import CalendarMonth from './CalendarMonth'
 import dayjs from "dayjs"
-import {useState, useRef, useEffect} from 'react'
+import {useState, useRef, useLayoutEffect} from 'react'
+import { FixedSizeList as List } from 'react-window'
+import type { ListChildComponentProps } from 'react-window'
 
-const months = Array.from({ length: 12 }, (_, i) => {
-  const month = dayjs().startOf("year").month(i);
-  const daysCount = month.daysInMonth();
-  
-    return {
-        label: month.format("MMMM"),
-        offset: (month.startOf("month").day() + 6) % 7,
-        days: Array.from({ length: daysCount }, (_, d) =>
-            month.date(d + 1).format("YYYY-MM-DD")
-        ),
-    }
-})
 
+const TOTAL_MONTHS = 3000
+const CENTER_INDEX = Math.floor(TOTAL_MONTHS / 2)
+
+function getMonth(index: number) {
+  const date = dayjs().add(index - CENTER_INDEX, "month")
+
+  const daysCount = date.daysInMonth()
+
+  return {
+    label: date.format("MMMM YYYY"),
+    offset: (date.startOf("month").day() + 6) % 7,
+    days: Array.from({ length: daysCount }, (_, d) =>
+      date.date(d + 1).format("YYYY-MM-DD")
+    )
+  }
+}
 
 export default function CalendarPage(){
-    const currentMonthRef = useRef<HTMLDivElement>(null)
     const [selectedDate, setSelectedDate] = useState<string>(dayjs().format("YYYY-MM-DD"))
-    const monthsComponents = months.map((month, i) => <CalendarMonth key={month.label} innerRef={i === dayjs().month() ? currentMonthRef : undefined} monthName={month.label} dates={month.days} offset={month.offset} selectedDate={selectedDate} onSelectedDate={setSelectedDate}/>)
-    
-    useEffect(() => {
-        currentMonthRef.current?.scrollIntoView({ block: "start"})
+    const initialMonthIndex = CENTER_INDEX
+    const [height, setHeight] = useState(0)
+    const mainRef = useRef<HTMLDivElement>(null)
+
+    useLayoutEffect(() => {
+        if (!mainRef.current) return
+
+        const resizeObserver = new ResizeObserver(entries => {
+        for (const entry of entries) {
+            setHeight(entry.contentRect.height)
+        }
+        })
+
+        resizeObserver.observe(mainRef.current)
+
+        return () => resizeObserver.disconnect()
     }, [])
 
+    const Row = ({ index, style }: ListChildComponentProps) => {
+        const month = getMonth(index)
+        return (
+            <div style={style}>
+            <div
+                style={{
+                height: 390,
+                borderBottom: "0.2px solid #b7b7b7a0",
+                boxSizing: "border-box"
+                }}
+            >
+                <CalendarMonth
+                    monthName={month.label}
+                    dates={month.days}
+                    offset={month.offset}
+                    selectedDate={selectedDate}
+                    onSelectedDate={setSelectedDate}
+                />
+            </div>
+            </div>
+        )
+    }
+    
     return(
-        <div className="layout" style={{ overflowY: 'auto'}}>
+        <div className="layout" style={{ height: "100vh", overflow: "hidden" }}>
             <header>
                 <NavLink className="header-btn" to="/">
                     <FaChevronLeft size={16} color="black" />
@@ -37,11 +77,22 @@ export default function CalendarPage(){
                 <div style={{width: '16px'}}></div>
             </header>
             <div className="calendar-page-day-bar">
-                {["M", "T", "W", "T", "F", "S", "S" ].map(d => <div>{d}</div>)}
+                {["M", "T", "W", "T", "F", "S", "S" ].map((d, idx) => <div key={idx}>{d}</div>)}
             </div>
-            <main style={{height: 0}}>
-                <div className="calendar-page-content">{monthsComponents}</div>
+            <main ref={mainRef} style={{ flex: 1 }}>
+                {height > 0 && (
+                        <List
+                            height={height}
+                            itemCount={TOTAL_MONTHS}
+                            itemSize={390}
+                            initialScrollOffset={initialMonthIndex * 390}
+                            width="100%"
+                        >
+                            {Row}
+                        </List>
+                    )}
             </main>
         </div>
     )
 }
+
