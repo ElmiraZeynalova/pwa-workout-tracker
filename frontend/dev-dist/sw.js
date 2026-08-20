4f0bdbbb44205d52e3e4d5efbd20cf898a88386d
ElmiraZeynalova <150475421+ElmiraZeynalova@users.noreply.github.com>
2026-08-20 09:57:09 +0200

    chore: initialize backend project with Express, TypeScript, Prisma

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -67,7 +67,7 @@ if (!self.define) {
     });
   };
 }
-define(['./workbox-5119daf5'], (function (workbox) { 'use strict';
+define(['./workbox-eb0a64e1'], (function (workbox) { 'use strict';
 
   self.skipWaiting();
   workbox.clientsClaim();
@@ -82,7 +82,7 @@ define(['./workbox-5119daf5'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.22opag6a5f"
+    "revision": "0.0a5q6a35t7g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
